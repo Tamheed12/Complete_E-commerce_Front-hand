6e804d480ad9424d5a665bb613ddeb6c5ff5ba4e
Tamheed Tariq <tamheedtariq2005@gmail.com>
2024-12-16 23:16:33 +0500

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,28 @@
 import Faq from "./components/Faq";
-// import Welcome from "./components/Welcome";
-// import Productpage from "./components/Productpage";
-// import Single from "./components/Single";
-// import Cart from "./components/Cart";
-// import Contact from "./components/Contact";
-// import About from "./components/About";
+import Welcome from "./components/Welcome";
+import Productpage from "./components/Productpage";
+import Single from "./components/Single";
+import Cart from "./components/Cart";
+import Contact from "./components/Contact";
+import About from "./components/About";
 
 export default function Home() {
   return (
     <main>
-      {/* <Welcome/> */}
+      <Welcome/>
 
       <Faq/>
 
-    {/* <Productpage /> */}
+    <Productpage />
 
-    {/* <Single/> */}
+    <Single/>
 
-    {/* <Cart/> */}
+    <Cart/>
   
 
-      {/* <Contact/> */}
+      <Contact/>
    
-        {/* <About/> */}
+        <About/>
     </main>
   );
 }
